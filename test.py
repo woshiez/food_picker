4a9d76c2aa4c4c41a4ab54790e25e4891dde79be
woshiez <enzezhao@gmail.com>
2024-01-23 23:56:09 -0800

    updated into python3, reliability fixes, optimization

--- test.py
+++ test.py
@@ -2,17 +2,16 @@
 import random
 
 Places = []
-morestuff = True
 
-while 	morestuff == True:
-  	newplace = raw_input("add new choices? 'no' to stop: ")
-    	if (newplace != "no"):	
-      		Places.append(newplace)
-    	else:
-      		morestuff = False
+while True:
+  	newplace = input("add new choices? (enter 'no' to stop): ")
+  	if (newplace != "no"):	
+  	    Places.append(newplace)
+  	else:
+  	    break
 
-print(Places[random.randint(0,len(Places)-1)])
-print(len(Places)-1)
-
-
-
+if not Places:
+    print("can't pick if you don't enter any choices")
+else:
+    choice = Places[random.randint(0,len(Places)-1)]
+    print(f"The food AI insists that you eat: {choice}")
